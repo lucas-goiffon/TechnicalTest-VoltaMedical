0ee81b0682568f047656e1bfb8daa361415d9a73
LuCas Goiffon <lucas.goiffon@epitech.eu>
2023-06-19 01:12:17 +0200

    Changing for scss. TODO: create and use variables

--- src/renderer/Home.tsx
+++ src/renderer/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Home.css';
+import './Home.scss';
 import Toggle from './components/Toggle/Toggle';
 
 type Alarm = {
--- src/renderer/components/Toggle/Toggle.tsx
+++ src/renderer/components/Toggle/Toggle.tsx
@@ -1,4 +1,4 @@
-import './Toggle.css';
+import './Toggle.scss';
 
 type ToggleProps = {
   checked?: boolean;
